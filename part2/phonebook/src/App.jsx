83d07153f322a3bd9b21d0ae601e9c3b6eca7ee3
John <john.d.lee.au@gmail.com>
2025-08-12 15:32:04 +1000

    Complete exercises 2.9-2.10: Implement filter and refactor code extracting Filter, Display, and Form components from App

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,60 @@
 import { useState } from 'react'
 
+const Filter = ({ handler }) => {
+  return (
+    <div>
+      filter shown with <input onChange={handler}/>
+    </div>
+  )
+}
+
+const Form = ({ onSubmit, onNameChange, onNumberChange }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        name: <input onChange={onNameChange}/>
+      </div>
+      <div>
+        number: <input onChange={onNumberChange}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Display = ({ visiblePersons }) => <ul>{visiblePersons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}</ul>
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const visiblePersons = persons.filter(person => 
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
   const handleNewName = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+
   const handleNewNumber = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterName = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -23,26 +65,17 @@ const App = () => {
     } else {
       setPersons(persons.concat({name : newName, number: newNumber}))
     }
-    
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input onChange={handleNewName}/>
-        </div>
-        <div>
-          number: <input onChange={handleNewNumber}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
+      <Filter handler={handleFilterName}/>
+      <h3>add a new</h3>
+      <Form onSubmit={addPerson} onNameChange={handleNewName} onNumberChange={handleNewNumber}/>
+      <h3>Numbers</h3>
       <div>debug: {newName} {newNumber}</div>
-      <ul>{persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}</ul>
+      <Display visiblePersons={visiblePersons}/>
     </div>
   )
 }
