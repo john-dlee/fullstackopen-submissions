3317a834a5f652617102a01e3f90696ecca57d8d
John <john.d.lee.au@gmail.com>
2025-08-20 13:05:06 +1000

    Complete exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,17 +1,63 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
-function App() {
+const Display = ({ countries }) => {
+  if (countries.length > 10) {
+    return <p> Too many matches, specify another filter</p>
+  } 
+  
+  if (countries.length === 1) {
+    const country = countries[0]
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <div>Capital {country.capital[0]}</div>
+        <div>Area {country.area}</div>
+        <h2>Languages</h2>
+        <ul>
+          {Object.values(country.languages).map(language => 
+            <li key={language}>
+              {language}
+            </li>)}
+        </ul>
+        <img 
+          src={country.flags.png}
+        />
+      </div>
+    )
+  }
 
+  return (
+    <ul>
+      {countries.map(country => 
+        <li key={country.cca3}>{country.name.common}</li>)}
+    </ul>
+  )
+}
+
+const App = ()  => {
   const [input, setInput] = useState(null)
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+    .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+    .then(response => setCountries(response.data))
+  }, [])
 
   const handleInput = (e) => {
     console.log(e.target.value)
     setInput(e.target.value)
   }
-
+  
+  const filteredCountries = countries.filter(country => 
+    country.name.common.toLowerCase().includes(input))
+  
+  console.log(filteredCountries)
   return (
     <div>
       find countries <input onChange={handleInput}/>
+      <Display countries={filteredCountries}/>
     </div>
   )
 }
