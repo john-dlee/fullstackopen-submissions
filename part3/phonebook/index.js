6532f10574b061f78cbb0d1e628ae04553e8f2ca
John <john.d.lee.au@gmail.com>
2025-08-25 15:35:39 +1000

    Add more post requests for testing

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,13 +26,13 @@ let notes = [
   }
 ]
 
-app.get('/api/persons', (request, response) => {
-  response.json(notes)
-})
-
 const generateId = () => {
   return String(Math.floor(Math.random() * 1000000) + 1)
 }
+
+app.get('/api/persons', (request, response) => {
+  response.json(notes)
+})
 
 app.get('/info', (request, response) => {
   const now = new Date()
