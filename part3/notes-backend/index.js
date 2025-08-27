e25291d62fa4c477308f96ebf6f946acbc639f6b
John <john.d.lee.au@gmail.com>
2025-08-27 15:28:11 +1000

    Fix error: access app before init

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 
+const app = express()
 app.use(express.static('dist'))
 app.use(cors())
-const app = express()
 
 let notes = [
   {
